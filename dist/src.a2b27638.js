dd37a3f0841e79a976a884d42c494a6fb6a9265c
koru_05 <72614335+KoudaiYokoyama@users.noreply.github.com>
2023-07-17 06:31:43 +0000

    map, filterを使った配列の処理

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -118,50 +118,44 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"src/index.js":[function(require,module,exports) {
-var _console;
-function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
-function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
-function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
 /**
- * デフォルト値、引数など
+ * maplやfilterを使った配列の処理
  */
-var sayHello = function sayHello() {
-  var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "default";
-  return console.log("Hello, ".concat(name));
-};
-sayHello();
+var nameArr = ["adam", "bob", "charry"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(nameArr[index]);
+// }
 
-/**
- * スプレッド構文 ...
- */
-// // 配列の展開
-// const arr1 = [1, 2, 3];
-// console.log(arr1); // [1, 2]
-// console.log(...arr1); // 1 2
+// // mapは処理が書き込める配列。下の例ではnameArrの要素がひとつずつnameに入る
+// const nameArr2 = nameArr.map((name) => {
+//   if (name != "bob") return name;
+//   else return "boby";
+// });
+// console.log(nameArr2); // ['adam', 'boby', 'charry']
 
-// const sumFunc = (num1, num2) => console.log(num1 + num2);
-// sumFunc(arr1[0], arr1[1]); // 3
-// sumFunc(...arr1); // 3
+//第２引数を指定すると、添え字が入る
+nameArr.map(function (name, index) {
+  return console.log("".concat(index, "\u756A\u76EE\u306F").concat(name, "\u3067\u3059\u3002"));
+}); //ひとつずつ出力する
+// 0番目はadamです。
+// index.js:17 1番目はbobです。
+// index.js:17 2番目はcharryです。
 
-// まとめる
-var arr2 = [1, 2, 3, 4, 5];
-var num1 = arr2[0],
-  num2 = arr2[1],
-  arr3 = arr2.slice(2); //分割代入しなかった値がarr3に代入される
-console.log(num1);
-console.log(num2);
-(_console = console).log.apply(_console, _toConsumableArray(arr3));
+// // filterはある条件に一致したものだけを返却し、新しい配列を作成する
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 == 1; //filterではreturn文に条件式を書く
+// });
+// console.log(newNumArr);// [1, 3, 5]
 
-// 配列のコピー、結合
-var arr4 = [10, 20];
-var arr5 = [30, 40];
-var arr6 = [].concat(arr4); //arr4と全く同じ配列
-console.log(arr6); //[10, 20]
-var arr7 = [].concat(arr4, arr5);
-console.log(arr7); //[10, 20, 30, 40]
+var newNameArr = nameArr.map(function (name) {
+  if (name === "adam") {
+    return name;
+  } else {
+    return "".concat(name, "\u3055\u3093");
+  }
+});
+console.log(newNameArr); // ['adam', 'bobさん', 'charryさん']
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- src/index.js
+++ src/index.js
@@ -1,32 +1,36 @@
 /**
- * デフォルト値、引数など
+ * maplやfilterを使った配列の処理
  */
-const sayHello = (name = "default") => console.log(`Hello, ${name}`);
-sayHello();
+const nameArr = ["adam", "bob", "charry"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(nameArr[index]);
+// }
 
-/**
- * スプレッド構文 ...
- */
-// // 配列の展開
-// const arr1 = [1, 2, 3];
-// console.log(arr1); // [1, 2]
-// console.log(...arr1); // 1 2
+// // mapは処理が書き込める配列。下の例ではnameArrの要素がひとつずつnameに入る
+// const nameArr2 = nameArr.map((name) => {
+//   if (name != "bob") return name;
+//   else return "boby";
+// });
+// console.log(nameArr2); // ['adam', 'boby', 'charry']
 
-// const sumFunc = (num1, num2) => console.log(num1 + num2);
-// sumFunc(arr1[0], arr1[1]); // 3
-// sumFunc(...arr1); // 3
+//第２引数を指定すると、添え字が入る
+nameArr.map((name, index) => console.log(`${index}番目は${name}です。`)); //ひとつずつ出力する
+// 0番目はadamです。
+// index.js:17 1番目はbobです。
+// index.js:17 2番目はcharryです。
 
-// まとめる
-const arr2 = [1, 2, 3, 4, 5];
-const [num1, num2, ...arr3] = arr2; //分割代入しなかった値がarr3に代入される
-console.log(num1);
-console.log(num2);
-console.log(...arr3);
+// // filterはある条件に一致したものだけを返却し、新しい配列を作成する
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 == 1; //filterではreturn文に条件式を書く
+// });
+// console.log(newNumArr);// [1, 3, 5]
 
-// 配列のコピー、結合
-const arr4 = [10, 20];
-const arr5 = [30, 40];
-const arr6 = [...arr4]; //arr4と全く同じ配列
-console.log(arr6); //[10, 20]
-const arr7 = [...arr4, ...arr5];
-console.log(arr7); //[10, 20, 30, 40]
+const newNameArr = nameArr.map((name) => {
+  if (name === "adam") {
+    return name;
+  } else {
+    return `${name}さん`;
+  }
+});
+console.log(newNameArr); // ['adam', 'bobさん', 'charryさん']
